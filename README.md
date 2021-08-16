072ceb45a5fb8eccc2daf0aa1154758e02da327a
adi <anpwr8@mail.missouri.edu>
2021-08-15 21:53:36 -0500

    Adding listener

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # fuck-yt-ads
 Wanna auto skip yt ads
 
-This should work for both chromium based browsers and firefox
+This should work for both chromium based browsers and firefox(ensure the processor version and the binary of the browser are the same. 
+(i.e.x64 has 64 bit binary)
 
 1. Does the URL contain YT ? 
 2. Get the DOM on the page
--- myScript.js
+++ myScript.js
@@ -1,7 +1,22 @@
-function state() { 
-    console.log("State Changed!"); 
+chrome.runtime.sendMessage({greeting: "this is from the sendMessage"}, function(response) {
+    console.log("this is from the sendmessage");
+});
+
+function reload(){
+    var adElement = document.getElementById("ad-text:3")
+    if(typeof(adElement) != 'undefined' && adElement != null){
+        console.log('Ad Element exists');
+    } else{
+        console.log('Ad Element does not exist');
+    }
 }
-var player = document.getElementById("movie_player");
-// player.addEventListener("onStateChange", "state");
-console.log("Started!");
-state();
+
+// TODO: So this listener is not being invoked.
+
+chrome.tab.onUpdated.addListener({greeting: "this is in the add listener"},function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete' && tab.active) {
+        console.log(" Is this a new tab,page");
+      }
+})
+
+// chrome.runtime.onUpdated
